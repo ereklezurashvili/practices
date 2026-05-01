6c3c9243aaffafaf193f297c4dbe89721c6754df
ereklezurashvili <ereklezurashvili@gmail.com>
2026-05-01 19:46:00 +0400

    Added FileService

--- practices/practice 14/Services/FileService.cs
+++ practices/practice 14/Services/FileService.cs
@@ -1,12 +1,89 @@
-﻿using System;
+using practice_14.Enums;
+using practice_14.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace practice_15.Services
+
+namespace practice_14.Services;
+
+public class FileService
 {
-    internal class FileService
+    private string taskPath = "Tasks.txt";
+    private string logPath = "Logs.txt";
+
+    // 📖 Read Tasks
+    public List<Models.Task> ReadTasks()
     {
+        List<Models.Task> tasks = new List<Models.Task>();
+
+        if (!File.Exists(taskPath))
+            return tasks;
+
+        string[] lines = File.ReadAllLines(taskPath);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+
+            Models.Task task = new Models.Task();
+            Guid id;
+            Guid.TryParse(parts[0], out id);
+            task.Id = id;
+            task.Title = parts[1];
+            task.Description = parts[2];
+            task.Status = (Enums.TaskStatus)Enum.Parse(typeof(Enums.TaskStatus), parts[3]);
+            task.CreatedAt = DateTime.Parse(parts[4]);
+
+            tasks.Add(task);
+        }
+
+        return tasks;
+    }
+
+    public void WriteTasks(List<Models.Task> tasks)
+    {
+        List<string> lines = new List<string>();
+
+        foreach (Models.Task t in tasks)
+        {
+            string line = t.Id + "|" + t.Title + "|" + t.Description + "|" + t.Status + "|" + t.CreatedAt;
+            lines.Add(line);
+        }
+
+        File.WriteAllLines(taskPath, lines);
+    }
+
+   
+    public List<Log> ReadLogs()
+    {
+        List<Log> logs = new List<Log>();
+
+        if (!File.Exists(logPath))
+            return logs;
+
+        string[] lines = File.ReadAllLines(logPath);
+
+        foreach (string line in lines)
+        {
+            Log log = new Log();
+            log.Message = line;
+            logs.Add(log);
+        }
+
+        return logs;
+    }
+
+    public void WriteLogs(List<Log> logs)
+    {
+        List<string> lines = new List<string>();
+
+        foreach (Log l in logs)
+        {
+            lines.Add(l.Message);
+        }
+
+        File.WriteAllLines(logPath, lines);
     }
 }
