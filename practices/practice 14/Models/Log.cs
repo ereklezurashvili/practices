c93bf8f073d4ae5063e21fde4b088e4399ebf43e
ereklezurashvili <ereklezurashvili@gmail.com>
2026-05-01 10:11:11 +0400

    Added Log.cs

--- practices/practice 14/Models/Log.cs
+++ practices/practice 14/Models/Log.cs
@@ -1,12 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practice_15.Models
+namespace practice_14.Models;
+
+internal class Log
 {
-    internal class Log
-    {
-    }
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TaskStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
