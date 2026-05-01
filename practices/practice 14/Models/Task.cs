2a362f0a454fd828c4007c96f742b19082263452
ereklezurashvili <ereklezurashvili@gmail.com>
2026-05-01 10:11:37 +0400

    Added Task.cs

--- practices/practice 14/Models/Task.cs
+++ practices/practice 14/Models/Task.cs
@@ -1,12 +1,20 @@
-﻿using System;
+using practice_14.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace practice_15.Models
+namespace practice_14.Models;
+
+
+internal class Task
 {
-    internal class Task
-    {
-    }
+    public Guid Id { get; set; }
+    public Guid TaskId { get; set; }
+    public ActionType ActionType { get; set; }
+    public string Message { get; set; }
+    public DateTime TimeStamp { get; set; }
 }
+
