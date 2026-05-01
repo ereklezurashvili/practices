7e480a9ca061f1cf6c9b16e29de0bf149d432d98
ereklezurashvili <ereklezurashvili@gmail.com>
2026-05-01 09:38:45 +0400

    Added Enums

---
+++ practices/practice 14/Models/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_15.Models
+{
+    internal class Task
+    {
+    }
+}
