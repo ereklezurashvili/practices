2a96b706fc1bab654029db9216a63bbc0c5a51d9
ereklezurashvili <ereklezurashvili@gmail.com>
2026-05-08 15:58:39 +0400

    Update Task.cs

--- practices/practice 14/Models/Task.cs
+++ practices/practice 14/Models/Task.cs
@@ -1,20 +1,48 @@
+
 using practice_14.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace practice_14.Models;
 
 
-internal class Task
+public class Task
 {
     public Guid Id { get; set; }
-    public Guid TaskId { get; set; }
-    public ActionType ActionType { get; set; }
-    public string Message { get; set; }
-    public DateTime TimeStamp { get; set; }
+
+    public string Title { get; set; }
+
+    public string Description { get; set; }
+
+    public  Enums.TaskStatus Status { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public Task()
+    {
+        // ONLY for deserialization
+    }
+
+
+    public Task(string title, string description)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        Status = Enums.TaskStatus.ToDo;
+        CreatedAt = DateTime.Now;
+    }
+
+    public override string ToString()
+    {
+        return
+            "Id: " + Id + "\n" +
+            "Title: " + Title + "\n" +
+            "Description: " + Description + "\n" +
+            "Status: " + Status + "\n" +
+            "Created At: " + CreatedAt;
+    }
 }
 
